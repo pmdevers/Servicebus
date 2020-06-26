8cbf5f3b3698c39494338c12dd1258d840d9f2f5
Patrick Evers <pmdevers@hotmail.com>
2020-06-26 17:06:08 +0200

    Add Sync Extensions

---
+++ src/pmdevers.servicebus/ServiceBusExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using PMDEvers.Servicebus.Interfaces;
+
+namespace PMDEvers.Servicebus
+{
+    public static class ServiceBusExtensions
+    {
+        private static readonly TaskFactory Factory = new TaskFactory(CancellationToken.None,
+            TaskCreationOptions.None, TaskContinuationOptions.None, TaskScheduler.Default);
+
+        private static void RunSync(Func<Task> func)
+        {
+            Factory.StartNew(func).Unwrap().GetAwaiter().GetResult();
+        }
+
+        private static TResult RunSync<TResult>(Func<Task<TResult>> func)
+        {
+            return Factory.StartNew(func).Unwrap().GetAwaiter().GetResult();
+        }
+
+        public static void Send(this IServiceBus serviceBus, ICommand command) =>
+            RunSync(() => serviceBus.SendAsync(command));
+
+        public static T Send<T>(this IServiceBus serviceBus, ICommand<T> command) =>
+            RunSync<T>(() => serviceBus.SendAsync<T>(command));
+
+        public static void Publish(this IServiceBus serviceBus, IEvent @event) =>
+            RunSync(() => serviceBus.PublishAsync(@event));
+        
+        public static T Query<T>(this IServiceBus serviceBus, IQuery<T> query) =>
+            RunSync<T>(() => serviceBus.QueryAsync<T>(query));
+
+    }
+
+}
